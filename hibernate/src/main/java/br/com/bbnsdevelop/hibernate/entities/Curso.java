d09171dbf63967bb760813a605f1066a07e3266f
bbnsdevelop <bbnsdevelop@gmail.com>
2022-04-13 22:26:27 -0300

    gerar id de forma automatica

--- hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/Curso.java
+++ hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/Curso.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Curso {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name ="nome", nullable = false, length = 50)
