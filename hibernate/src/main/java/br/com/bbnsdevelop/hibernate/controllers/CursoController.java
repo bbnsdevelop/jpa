f349ae9358d19ca4f0cc296b888b59de32ae4d2b
bbnsdevelop <bbnsdevelop@gmail.com>
2022-04-13 22:25:00 -0300

    salvando e fazendo um update no curso

--- hibernate/src/main/java/br/com/bbnsdevelop/hibernate/controllers/CursoController.java
+++ hibernate/src/main/java/br/com/bbnsdevelop/hibernate/controllers/CursoController.java
@@ -1,8 +1,12 @@
 package br.com.bbnsdevelop.hibernate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,9 @@ public class CursoController {
 	public Curso getById(@PathVariable("id") Long id) {
 		return repository.findById(id);
 	}
-
+		
+	@PostMapping
+	public ResponseEntity<Curso> save(@RequestBody Curso curso) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(curso));
+	}
 }
