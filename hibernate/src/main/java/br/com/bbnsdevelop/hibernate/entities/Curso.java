361f73798c9afff07bcf087bab4d7b58fa3e2610
bbnsdevelop <bbnsdevelop@gmail.com>
2022-04-25 19:14:03 -0300

    native query

--- hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/Curso.java
+++ hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/Curso.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
@@ -18,28 +19,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="TB_CURSO")
+@Table(name = "TB_CURSO")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@NamedQuery(name ="query_get_all_cursos", query = "select c from Curso c")
-@NamedQuery(name ="query_get_curso_by_id", query = "select c from Curso c where c.id = :id")
+@NamedQueries(value = { 
+		@NamedQuery(name = "query_get_all_cursos", query = "select c from Curso c"),
+		@NamedQuery(name = "query_get_curso_by_id", query = "select c from Curso c where c.id = :id") 
+		})
 public class Curso {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(name ="nome", nullable = false, length = 50)
+
+	@Column(name = "nome", nullable = false, length = 50)
 	private String nome;
-	
+
 	@CreationTimestamp
 	@Column(name = "dh_criacao", nullable = false)
 	private LocalDateTime dataCriacao;
-	
+
 	@UpdateTimestamp
 	@Column(name = "dh_atualizacao", nullable = true)
 	private LocalDateTime ultimaAtualizacao;
-	
 
 }
