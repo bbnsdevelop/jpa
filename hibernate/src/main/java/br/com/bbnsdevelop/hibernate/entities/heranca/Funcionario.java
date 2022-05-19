a5d8589ad9c175e7eb8b0e3900b0c35c43c0a904
bbnsdevelop <bbnsdevelop@gmail.com>
2022-05-18 22:08:47 -0300

    Nome da coluna - @DiscriminatorColumn(name = funcionarioType)

--- hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/heranca/Funcionario.java
+++ hibernate/src/main/java/br/com/bbnsdevelop/hibernate/entities/heranca/Funcionario.java
@@ -1,6 +1,7 @@
 package br.com.bbnsdevelop.hibernate.entities.heranca;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "funcionarioType")
 public class Funcionario {
 
 	
