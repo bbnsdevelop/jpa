4309753b203c584c3b78563dadee70f2f5aae624
bbnsdevelop <bbnsdevelop@gmail.com>
2022-06-18 19:29:41 -0300

    spring data rest

---
+++ springDataJPAeSpringDataRest/src/main/java/br/com/bbnsdevelop/jpaESpringDataRest/controllers/jpadatarest/CursoRepositoryJpaDataRest.java
@@ -0,0 +1,17 @@
+package br.com.bbnsdevelop.jpaESpringDataRest.controllers.jpadatarest;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import br.com.bbnsdevelop.jpaESpringDataRest.entities.Curso;
+
+@RepositoryRestResource(path = "/cursos")
+public interface CursoRepositoryJpaDataRest extends JpaRepository<Curso, Long> {
+
+	public List<Curso> findByNome(String nome);
+
+	
+
+}
